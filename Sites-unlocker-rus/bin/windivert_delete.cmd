352f6c4fed2c2ef1be813db3248b4b042cf78b12
MrMyth <starchikov92@mail.ru>
2025-06-23 12:41:24 +0300

    Обновил ядро или основу сборки

--- Sites-unlocker-rus/bin/windivert_delete.cmd
+++ Sites-unlocker-rus/bin/windivert_delete.cmd
@@ -1,19 +1,20 @@
 @echo off
+
 if "%1%" == "del" (
-    echo DELETE WINDIVERT DRIVER
-    sc delete windivert
-    sc stop windivert
-    goto :end
+	echo DELETE WINDIVERT DRIVER
+	sc delete windivert
+	sc stop windivert
+	goto :end
 )
 
 sc qc windivert
 if errorlevel 1 goto :end
 
 echo.
-choice /C YN /M "Do you want to stop and delete windivert (y/n)? " /D N /T 30
-
+choice /C YN /M "Do you want to stop and delete windivert"
 if ERRORLEVEL 2 goto :eof
-wscript tools\elevator.vbs "%0" del
+
+"%~dp0elevator" %0 del
 goto :eof
 
 :end
